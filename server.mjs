7161b36ca47f23da8a0a30ac0c0bcd8ae70bb7b0
yohannbadin13010-ship-it <yohann.badin13010@gmail.com>
2026-08-13 17:40:11 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET || "CHANGE_ME";
 const piscine = new Piscine({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: PRocess.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL?.includes("supabase")
     ? { rejectUnauthorized: false }
     : undefined
